2054365cc483048be79633fba6f23ea1879208a0
Dima <dmmhnk23@gmail.com>
2024-06-11 23:13:55 +0200

    first commit

---
+++ src/main/java/mate/academy/StringBootIntro/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package mate.academy.StringBootIntro.repository;
+
+import mate.academy.StringBootIntro.model.Book;
+import java.util.List;
+
+public interface BookRepository {
+    Book save(Book book);
+    List findAll();
+}
---
+++ src/main/java/mate/academy/StringBootIntro/service/BookService.java
@@ -0,0 +1,10 @@
+package mate.academy.StringBootIntro.service;
+
+import mate.academy.StringBootIntro.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+    List findAll();
+}
