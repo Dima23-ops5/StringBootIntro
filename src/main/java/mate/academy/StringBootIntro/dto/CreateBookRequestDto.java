7cf5ebf6806a64ec312ff5278225212b68eb6ab3
Dima <dmmhnk23@gmail.com>
2024-06-29 18:07:10 +0200

    added globalExeptionHandler and validation for classes dto

--- src/main/java/mate/academy/StringBootIntro/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/StringBootIntro/dto/CreateBookRequestDto.java
@@ -1,14 +1,22 @@
 package mate.academy.StringBootIntro.dto;
 
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
     private BigDecimal price;
+    @NotNull
     private String description;
+    @NotNull
     private String coverImage;
 }
