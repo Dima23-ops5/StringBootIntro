aed1ed89704d17219cf52c2df193726d32a543a1
Dima <dmmhnk23@gmail.com>
2024-06-30 11:36:38 +0200

    removed validation in class BookDto and added annotations

--- src/main/java/mate/academy/StringBootIntro/dto/BookDto.java
+++ src/main/java/mate/academy/StringBootIntro/dto/BookDto.java
@@ -1,24 +1,15 @@
 package mate.academy.StringBootIntro.dto;
 
 import java.math.BigDecimal;
-
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class BookDto {
-    @NotNull
     private Long id;
-    @NotNull
     private String title;
-    @NotNull
     private String author;
-    @NotNull
     private String isbn;
-    @NotNull
     private BigDecimal price;
-    @NotNull
     private String description;
-    @NotNull
     private String coverImage;
 }
--- src/main/java/mate/academy/StringBootIntro/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/StringBootIntro/dto/CreateBookRequestDto.java
@@ -1,22 +1,27 @@
 package mate.academy.StringBootIntro.dto;
 
 import java.math.BigDecimal;
-
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
+    @NotNull(message = "Title can`t be null")
     private String title;
-    @NotNull
+    @NotBlank
+    @NotNull(message = "Author can`t be null")
     private String author;
-    @NotNull
+    @NotBlank
+    @NotNull(message = "Isbn can`t be null")
     private String isbn;
-    @NotNull
+    @NotNull(message = "Price can`t be null and also will be greater than 0")
     private BigDecimal price;
-    @NotNull
+    @NotBlank
+    @NotNull(message = "Description can`t be null")
     private String description;
-    @NotNull
+    @NotBlank
+    @NotNull(message = "CoverImage can`t be null")
     private String coverImage;
 }
