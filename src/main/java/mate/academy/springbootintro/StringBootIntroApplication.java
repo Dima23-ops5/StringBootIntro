dbeace74da9beb4e3a9490380abb3af293c54d20
Dima <dmmhnk23@gmail.com>
2024-07-31 13:21:29 +0200

    remove file .env in commit and add whiteline in the end of files

--- src/main/java/mate/academy/springbootintro/StringBootIntroApplication.java
+++ src/main/java/mate/academy/springbootintro/StringBootIntroApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StringBootIntroApplication {
 	public static void main(String[] args) {
-    	SpringApplication.run(StringBootIntroApplication.class, args);
+		SpringApplication.run(StringBootIntroApplication.class, args);
 	}
 
 }
--- src/main/java/mate/academy/springbootintro/controller/ShoppingCartController.java
+++ src/main/java/mate/academy/springbootintro/controller/ShoppingCartController.java
@@ -13,7 +13,6 @@ import mate.academy.springbootintro.service.shoppingcart.ShoppingCartService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
