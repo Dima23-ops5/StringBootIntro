f188e00d93add77f73514bb5aa87d2c8f5672ce0
Dima <dmmhnk23@gmail.com>
2024-08-03 10:13:13 +0200

    current changes

---
+++ src/test/java/mate/academy/springbootintro/repositoryTests/BookRepositoryTest.java
@@ -0,0 +1,34 @@
+package mate.academy.springbootintro.repositoryTests;
+
+import mate.academy.springbootintro.model.Book;
+import mate.academy.springbootintro.repository.book.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    @Sql(scripts = "classpath:database/book/create-book-with-category.sql",
+    executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "classpath:database/book/delete-book-with-category.sql",
+    executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void getBookByCategoryId_GetBookByExistingCategory_Correct() {
+        List<Book> books = bookRepository.getBooksByCategoryId(1L);
+
+        assertNotNull(books);
+        assertEquals(1, books.size());
+    }
+
+}
