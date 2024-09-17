4f43fbe1ef40d31a27ba85dea879c0bf912493dd
Dima <dmmhnk23@gmail.com>
2024-09-17 20:19:11 +0200

    remove all files

--- src/main/java/mate/academy/StringBootIntro/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package mate.academy.StringBootIntro.repository;
-
-import mate.academy.StringBootIntro.model.Book;
-import java.util.List;
-
-public interface BookRepository {
-    Book save(Book book);
-    List findAll();
-}
