a14c0594cf86b03b5e9ecaf4485e8774359a28f8
Dima <dmmhnk23@gmail.com>
2024-09-17 20:46:10 +0200

    remove unused directory and add checkstyle

--- src/main/java/mate/academy/StringBootIntro/repository/SpecificationProvider.java
+++
@@ -1,8 +0,0 @@
-package mate.academy.StringBootIntro.repository;
-
-import org.springframework.data.jpa.domain.Specification;
-
-public interface SpecificationProvider<T> {
-    String getKey();
-    Specification<T> getSpecification(String[] params);
-}
